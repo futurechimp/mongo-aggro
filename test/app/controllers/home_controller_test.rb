a63748359bad13a78c2e594842da6eabf7786d4e
futurechimp <dave@toaster>
2011-05-25 18:38:03 +0100

    Testing Home#index with and without items created.

--- test/app/controllers/home_controller_test.rb
+++ test/app/controllers/home_controller_test.rb
@@ -2,13 +2,26 @@ require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
 
 class HomeControllerTest < Test::Unit::TestCase
   context "HomeController" do
-    setup do
-      get '/'
-    end
-
-    should "work" do
-      assert last_response.ok?
+    
+    context "with no FeedItems" do
+      setup do
+        get '/'
+      end
+      
+      should "work" do
+        assert last_response.ok?
+      end  
     end
     
+    context "with 1 FeedItem" do
+      setup do
+        FeedItem.make
+        get '/'
+      end
+      
+      should "work" do
+        assert last_response.ok?
+      end
+    end    
   end
 end
