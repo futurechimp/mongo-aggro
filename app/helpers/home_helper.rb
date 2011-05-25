462af8098da23edaf552792dddc47007584e8835
futurechimp <dave@toaster>
2011-05-25 18:31:50 +0100

    Added a page_param method to home_helper.rb

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,9 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Mongoaggro.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  # A convenience method which either grabs the page param or returns 1
+  #
+  def page_param
+    (params[:page] ||= 1).to_i
+  end
 end
