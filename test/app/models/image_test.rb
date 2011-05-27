7f7d1abcb4d2c59c09b2ec245561ad6d3b4e5d5c
futurechimp <dave@toaster>
2011-05-27 09:59:02 +0100

    Starting to test image storage.

--- test/app/models/image_test.rb
+++ test/app/models/image_test.rb
@@ -2,11 +2,32 @@ require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
 
 class ImageTest < Test::Unit::TestCase
   context "Image Model" do
+
+    setup do
+      FeedItem.delete_all
+    end
     
     should 'construct new instance' do
       @image = Image.new
       assert_not_nil @image
     end
-    
+
+    context "upload" do
+      setup do
+        @feed_item = FeedItem.make(:with_feed_and_wire)
+        @image = Image.new
+        @feed_item.image = @image
+        
+        file = File.new PADRINO_ROOT + "/test/fixtures/omegaman.jpg"
+        @image.file = file
+        
+        @feed_item.save!
+      end
+      
+      should "work" do
+        assert_equal "omegaman.jpg", @image.file.filename
+      end
+      
+    end
   end
 end
