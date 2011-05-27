dc9e2551bc5932c668a897a044ec8afe545d829b
futurechimp <dave@toaster>
2011-05-27 09:07:13 +0100

    Added some of the default CarrierWave directives to the ImageUploader.

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -2,4 +2,53 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :grid_fs
 
+  ##
+  # Image manipulator library:
+  #
+  # include CarrierWave::RMagick
+  # include CarrierWave::ImageScience
+  # include CarrierWave::MiniMagick
+
+  ##
+  # Directory where uploaded temp files will be stored (default is [root]/tmp)
+  #
+  def cache_dir
+    Padrino.root("tmp")
+  end
+
+  ##
+  # Default URL as a default if there hasn't been a file uploaded
+  #
+  # def default_url
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  ##
+  # Process files as they are uploaded.
+  #
+  # process :resize_to_fit => [740, 580]
+
+  ##
+  # Create different versions of your uploaded files
+  #
+  # version :header do
+  #   process :resize_to_fill => [940, 250]
+  #   version :thumb do
+  #     process :resize_to_fill => [230, 85]
+  #   end
+  # end
+  ##
+  # White list of extensions which are allowed to be uploaded:
+  #
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  ##
+  # Override the filename of the uploaded files
+  #
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+
 end
