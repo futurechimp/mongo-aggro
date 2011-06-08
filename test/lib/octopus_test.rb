ccd57594d77c5a11d032e87272c1adf3a8cd2033
futurechimp <dave@toaster>
2011-06-08 16:24:56 +0100

    Asserting a Timeout::Error gets raised in the case of a timeout.  This actually is not a good code path, it's more like documentation to do something about it later.

--- test/lib/octopus_test.rb
+++ test/lib/octopus_test.rb
@@ -8,17 +8,32 @@ class OctopusTest < Test::Unit::TestCase
     end
 
     context "hitting the 'create_resource' method" do
-       setup do
-        @feed = Feed.make
-        stub_successful_net_resource_creation(@feed)
-        Octopus.create_resource(@feed)
-      end
+      context "when the octopus is running happily" do
+        setup do
+          @feed = Feed.make
+          stub_successful_net_resource_creation(@feed)
+          Octopus.create_resource(@feed)
+        end
 
-      should "post a '/create' request to the octopus" do
-        assert_requested :post, subscription_url, successful_subscription_request_body(@feed)
+        should "post a '/create' request to the octopus" do
+          assert_requested :post, subscription_url, successful_subscription_request_body(@feed)
+        end        
+      end
+      
+      context "when the octopus is not available" do
+        setup do
+          @feed = Feed.make      
+          stub_successful_net_resource_creation(@feed).to_timeout
+          assert_raise(Timeout::Error) do
+            Octopus.create_resource(@feed)
+          end
+        end
+        
+        should "raise an error" do
+          assert true
+        end
       end
     end
-
   end
 end
 
